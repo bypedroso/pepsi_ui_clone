413daf57eae13504fca996a6f5f4e8eb3913f333
Bruno Pedroso <brunooliveirapedroso@gmail.com>
2020-10-21 00:07:31 -0300

    create website

---
+++ main.js
@@ -0,0 +1,21 @@
+function imgSlider(imgSrc){
+  
+  document.querySelector('.pepsi').src = `images/${imgSrc}.png` 
+  
+  let color = "#0062be"
+  
+  if(imgSrc === 'pepsi001'){
+    color = "#0062be"
+  }else if(imgSrc === 'pepsi002'){
+    color = "#e60c2c"
+  }else if(imgSrc === 'pepsi003'){
+    color = "#1e1e1e"
+  }
+
+  changeBgColor(color)
+}
+
+function changeBgColor(color){
+  const sec = document.querySelector('.sec')
+  sec.style.background = color
+}
